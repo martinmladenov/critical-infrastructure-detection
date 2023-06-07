1f4baa12fd01a509cb13274cc4c74f2d232a0822
Martin Mladenov <m.mladenov@student.tudelft.nl>
2023-06-08 01:13:31 +0200

    Update AS script

--- other/as_count.py
+++ other/as_count.py
@@ -5,7 +5,7 @@ from sys import argv
 
 from tabulate import tabulate
 
-# This script can accept input from step 1+
+# This script can accept input from step 4
 def main():
     filename = argv[1]
     file = open(filename)
@@ -13,8 +13,16 @@ def main():
 
     asc = dict() # AS counts
     asd = dict() # AS names / descriptions
+
+    required_indications = [
+        'classification_likely_real',
+        'classification_potentially_real'
+    ]
     
     for host in hosts:
+        indications = host['indications']
+        if all(x not in required_indications for x in indications):
+            continue
         asn = host['asn']
         if not asn in asc:
             asc[asn] = 0
@@ -25,6 +33,7 @@ def main():
     rows = [[asn, asd[asn], asc[asn]] for asn in asc.keys()]
     rows = sorted(rows, key=lambda k: k[2], reverse=True)
 
+    print(tabulate([["ASN", "AS Name", "Count"]] + rows, headers="firstrow"))
     print(tabulate([["ASN", "AS Name", "Count"]] + rows, headers="firstrow", tablefmt='latex'))
     
     
